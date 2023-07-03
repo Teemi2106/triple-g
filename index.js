612445f2bffc23a303d351b417ec476d5a9e42c9
Teemi2106 <121306345+Teemi2106@users.noreply.github.com>
2023-07-03 18:01:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,3 +22,15 @@ function mySubmit(event) {
   localStorage.setItem(message);
   alert("We've recieved your message ", name);
 }
+
+window.addEventListener("load", function () {
+  setTimeout(function open(event) {
+    document.querySelector(".popup").style.display = "block";
+  }, 2000);
+});
+
+document.querySelector("#close").addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "none";
+});
+
+document.addEventListener("click", window.location("zoom.html"));
